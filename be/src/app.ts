dd258e8016a17505b5ea2a06c3f01d0848848145
abhijit sinha <abhijitinweb@gmail.com>
2025-12-16 20:45:28 +0530

    swagger-ui setup for API doc

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import eventsRoutes from "./modules/events/events.routes";
 import { errorHandler } from "./common/error-handler";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 import cors from "cors";
 
 const app = express();
@@ -16,7 +18,26 @@ app.use(
 
 app.use(express.json());
 
-app.use(express.json());
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Fleet Diagnostics API",
+      version: "1.0.0",
+      description: "API documentation",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/modules/events/*.ts"], // files containing annotations as above
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use("/events", eventsRoutes);
 
--- be/src/modules/events/events.routes.ts
+++ be/src/modules/events/events.routes.ts
@@ -1,8 +1,73 @@
-import { Router } from 'express';
-import { getEvents } from './events.controller';
+import { Router } from "express";
+import { getEvents } from "./events.controller";
 
 const router = Router();
 
-router.get('/', getEvents);
+/**
+ * @swagger
+ * /events:
+ *   get:
+ *     summary: Retrieve diagnostic events
+ *     parameters:
+ *       - in: query
+ *         name: vehicle_id
+ *         schema:
+ *           type: string
+ *         description: Vehicle ID
+ *       - in: query
+ *         name: code
+ *         schema:
+ *           type: string
+ *         description: Error code
+ *       - in: query
+ *         name: level
+ *         schema:
+ *            type: string
+ *            enum:
+ *             - INFO
+ *             - WARN
+ *             - ERROR
+ *             - CRITICAL
+ *         description: Severity
+ *       - in: query
+ *         name: from
+ *         required: false
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *         description: Start timestamp (ISO 8601, inclusive)
+ *
+ *       - in: query
+ *         name: to
+ *         required: false
+ *         schema:
+ *           type: string
+ *           format: date-time
+ *         description: End timestamp (ISO 8601, inclusive)
+ *     responses:
+ *       200:
+ *         description: List of diagnostic events
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   vehicle_id:
+ *                     type: string
+ *                   level:
+ *                     type: string
+ *                   code:
+ *                     type: string
+ *                   message:
+ *                     type: string
+ *                   timestamp:
+ *                     type: string
+ *                     format: date-time
+ */
+router.get("/", getEvents);
 
 export default router;
